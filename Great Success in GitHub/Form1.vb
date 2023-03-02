aeb27763fed1319cec0c44c4d0676e62f771872f
Itachi <Itachi@LAPTOP-9MV3EQH4>
2023-03-02 10:42:01 -0600

    Please work.

--- Great Success in GitHub/Form1.vb
+++ Great Success in GitHub/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
         lblOutput.Text = "First Line before update"
+
+        lblOutput.Text = "Second Line... update for me."
     End Sub
 End Class
